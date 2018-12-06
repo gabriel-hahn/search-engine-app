3d69374dd7c5daa85eae94916309688c75ebe3e9
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-12-06 14:49:47 -0200

    Add module config - Webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist'),
         publicPath: 'dist'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                }],
+            },
+        ],
+    },
 };
