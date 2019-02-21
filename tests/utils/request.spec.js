51d18876f2851ab0c8e3129da2709cafa17d268c
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-02-21 09:31:45 -0300

    Codacy code changes

--- tests/utils/request.spec.js
+++ tests/utils/request.spec.js
@@ -1,11 +1,11 @@
 // Needs polyfill because I used async functions.
-import 'babel-polyfill';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import "babel-polyfill";
+import { expect } from "chai";
+import sinon from "sinon";
 
-import RequestUtil from '../../src/utils/RequestUtil';
+import RequestUtil from "../../src/utils/RequestUtil";
 
-describe('Request Util', () => {
+describe("Request Util", () => {
     let requests = [];
     let URL = 'http://localhost:9090?term="Dog"';
 
@@ -22,90 +22,90 @@ describe('Request Util', () => {
         global.XMLHttpRequest.restore();
     });
 
-    describe('Request Util', () => {
-        describe('Smoke tests', () => {
-            it('Should exists get method', () => {
+    describe("Request Util", () => {
+        describe("Smoke tests", () => {
+            it("Should exists get method", () => {
                 expect(RequestUtil.get).to.exist;
             });
 
-            it('Should exists post method', () => {
+            it("Should exists post method", () => {
                 expect(RequestUtil.post).to.exist;
             });
 
-            it('Should exists put method', () => {
+            it("Should exists put method", () => {
                 expect(RequestUtil.put).to.exist;
             });
         });
 
-        describe('Request count', () => {
-            it('Should call request GET once', () => {
+        describe("Request count", () => {
+            it("Should call request GET once", () => {
                 RequestUtil.get(URL);
                 expect(requests.length).to.be.eq(1);
             });
 
-            it('Should call request GET twice', () => {
+            it("Should call request GET twice", () => {
                 RequestUtil.get(URL);
                 RequestUtil.get(URL);
                 expect(requests.length).to.be.eq(2);
             });
 
-            it('Should call request POST once', () => {
+            it("Should call request POST once", () => {
                 RequestUtil.post(URL);
                 expect(requests.length).to.be.eq(1);
             });
 
-            it('Should call request POST twice', () => {
+            it("Should call request POST twice", () => {
                 RequestUtil.post(URL);
                 RequestUtil.post(URL);
                 expect(requests.length).to.be.eq(2);
             });
 
-            it('Should call request PUT once', () => {
+            it("Should call request PUT once", () => {
                 RequestUtil.put(URL);
                 expect(requests.length).to.be.eq(1);
             });
 
-            it('Should call request PUT twice', () => {
+            it("Should call request PUT twice", () => {
                 RequestUtil.put(URL);
                 RequestUtil.put(URL);
                 expect(requests.length).to.be.eq(2);
             });
         });
 
-        describe('Return and request values', () => {
-            describe('GET method', () => {
-                it('Should make a request with correct URL', () => {
+        describe("Return and request values", () => {
+            describe("GET method", () => {
+                it("Should make a request with correct URL", () => {
                     RequestUtil.get(URL);
                     expect(requests[0].url).to.be.eq(URL);
                 });
 
-                it('Should calls the correct HTTP method', () => {
+                it("Should calls the correct HTTP method", () => {
                     RequestUtil.get(URL);
-                    expect(requests[0].method).to.be.eq('GET');
+                    expect(requests[0].method).to.be.eq("GET");
                 });
             });
 
-            describe('POST method', () => {
-                it('Should make a request with correct URL', () => {
+            describe("POST method", () => {
+                it("Should make a request with correct URL", () => {
                     RequestUtil.post(URL);
                     expect(requests[0].url).to.be.eq(URL);
                 });
 
-                it('Should calls the correct HTTP method', () => {
+                it("Should calls the correct HTTP method", () => {
                     RequestUtil.post(URL);
-                    expect(requests[0].method).to.be.eq('POST');
+                    expect(requests[0].method).to.be.eq("POST");
                 });
             });
 
-            describe('PUT method', () => {
-                it('Should make a request with correct URL', () => {
+            describe("PUT method", () => {
+                it("Should make a request with correct URL", () => {
                     RequestUtil.put(URL);
                     expect(requests[0].url).to.be.eq(URL);
                 });
 
-                it('Should calls the correct HTTP method', () => {
+                it("Should calls the correct HTTP method", () => {
                     RequestUtil.put(URL);
-                    expect(requests[0].method).to.be.eq('PUT');
+                    expect(requests[0].method).to.be.eq("PUT");
                 });
             });
         });
